b3741bfceb1ed129733de78b84ccccd31beb6257
Amin Farzanepey <aminfarzanepey@gmail.com>
2024-10-30 08:53:59 +0330

    add png file and python file that i wrote the move section and show already

---
+++ badman_with_img.py
@@ -0,0 +1,63 @@
+import pygame
+import random
+
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("badman.png").convert()
+        self.surf.set_colorkey((255,255,255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
+
+runing = True
+
+while runing:
+
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                runing = False
+
+        elif event.type == QUIT:
+            runing = False
+
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    screen.fill((0,0,0))
+    screen.blit(player.surf, player.rect)
+    pygame.display.flip()
