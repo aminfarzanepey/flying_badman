7ad9b8a99953c225abc697f72fec5a0f629b3af4
Amin Farzanepey <aminfarzanepey@gmail.com>
2024-11-06 14:52:34 +0330

    adding enemy class and sprite groups

--- badman_with_img.py
+++ badman_with_img.py
@@ -46,12 +46,49 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.bottom > SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
 
+
+#define enemy object
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("enemy.png").convert()
+        self.surf.set_colorkey((255,255,255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+        self.speed = random.randint(5,20)
+
+    #move the sprite based on speed
+    #remove the sprite when it passes the left edge of screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+#creates custom event for adding a new enemy
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY,250)
+
+#creates player
 player = Player()
 
+
+#creat groups to hold enemy sprites and all sprites
+#- enemies is used for collision detection and position update
+#- all_sprites is used for rendering
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+#variable to keep main loop runing
 runing = True
 
 while runing:
@@ -64,10 +101,23 @@ while runing:
         elif event.type == QUIT:
             runing = False
 
+        #add new enemy?
+        elif event.type == ADDENEMY:
+            # ceates the new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
 
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
 
+    #enemy update
+    enemies.update()
+
     screen.fill((0,0,0))
-    screen.blit(player.surf, player.rect)
+
+    #draw all sprites
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
     pygame.display.flip()
